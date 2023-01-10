cc412d5c72de6ed9019fd18e314479d5af786cba
Niv Fertig <nivfertig@gmail.com>
2023-01-10 20:48:44 +0200

    hh

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import todoList from "./todoList";
+
+function App() {
+  return (<p>gfjhgf</p>
+    // <>
+    //   <todoList/>
+    //   <input type="text"/>
+    //   <button>Add todo</button>
+    //   <button>Clear completed</button>
+    //   <div>0 missions left</div>  
+    // </>
+    )
+}
+export default App;
---
+++ src/todoList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class todoList extends Component {
+  render() {
+    return (
+      <div>
+        <p>adfljkhg</p>
+      </div>
+    )
+  }
+}
+
+export default todoList
